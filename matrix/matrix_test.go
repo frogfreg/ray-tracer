a4ee8f88f6827263456de6b63e338689e707bd8a
Guillermo Fernández <bengallme@gmail.com>
2024-08-10 19:38:13 -0600

    more tests for matrix package

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -200,18 +200,67 @@ func TestTranspose(t *testing.T) {
 }
 
 func TestDeterminant(t *testing.T) {
-	aMatString := `
+	aString := `
 | 1 | 5 |
 | -3 | 2 |
 `
 
-	a, err := MatrixFromString(aMatString)
+	a, err := MatrixFromString(aString)
 	if err != nil {
 		t.Error(err)
 	}
 
 	if a.Determinant() != 17 {
 		t.Error("the determinant should be 17")
+	}
+
+	aString = `
+| 1 | 2 | 6 |
+| -5 | 8 | -4 |
+| 2 | 6 | 4 |`
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if Cofactor(a, 0, 0) != 56 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 1) != 12 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 2) != -46 {
+		t.Error("wrong value")
+	}
+	if a.Determinant() != -196 {
+		t.Error("wrong value")
+	}
+
+	aString = `
+| -2 | -8 | 3 | 5 |
+| -3 | 1 | 7 | 3 |
+| 1 | 2 | -9 | 6 |
+| -6 | 7 | 7 | -9 |`
+
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if Cofactor(a, 0, 0) != 690 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 1) != 447 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 2) != 210 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 3) != 51 {
+		t.Error("wrong value")
+	}
+	if a.Determinant() != -4071 {
+		t.Error("wrong value")
 	}
 }
 
@@ -263,5 +312,207 @@ func TestSubmatrix(t *testing.T) {
 
 	if !AreEqual(Submatrix(a, 2, 1), expected) {
 		t.Error(err)
+	}
+}
+
+func TestMinor(t *testing.T) {
+	aString := `
+| 3 | 5 | 0 |
+| 2 | -1 | -7 |
+| 6 | -1 | 5 |`
+
+	a, err := MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := Submatrix(a, 1, 0)
+
+	if b.Determinant() != 25 {
+		t.Error("determinant is wrong!")
+	}
+
+	if Minor(a, 1, 0) != b.Determinant() {
+		t.Error("values should be the same")
+	}
+}
+
+func TestCofactor(t *testing.T) {
+	aString := `
+| 3 | 5 | 0 |
+| 2 | -1 | -7 |
+| 6 | -1 | 5 |`
+
+	a, err := MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if Minor(a, 0, 0) != -12 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 0, 0) != -12 {
+		t.Error("wrong value")
+	}
+	if Minor(a, 1, 0) != 25 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 1, 0) != -25 {
+		t.Error("wrong value")
+	}
+}
+
+func TestIsInvertible(t *testing.T) {
+	aString := `
+| 6 | 4 | 4 | 4 |
+| 5 | 5 | 7 | 6 |
+| 4 | -9 | 3 | -7 |
+| 9 | 1 | 7 | -6 |`
+
+	a, err := MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.Determinant() != -2120 || !a.IsInvertible() {
+		t.Error("matrix should be invertible")
+	}
+	aString = `
+| -4 | 2 | -2 | -3 |
+| 9 | 6 | 2 | 6 |
+| 0 | -5 | 1 | -5 |
+| 0 | 0 | 0 | 0 |`
+
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if a.Determinant() != 0 || a.IsInvertible() {
+		t.Error("matrix should not be invertible")
+	}
+}
+
+func TestInverse(t *testing.T) {
+	aString := `
+| -5 | 2 | 6 | -8 |
+| 1 | -5 | 1 | 8 |
+| 7 | 7 | -6 | -7 |
+| 1 | -3 | 7 | 4 |`
+
+	a, err := MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := a.Inverse()
+
+	if a.Determinant() != 532 {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 2, 3) != -160 {
+		t.Error("wrong value")
+	}
+
+	if b[3][2] != -(160.0 / 532.0) {
+		t.Error("wrong value")
+	}
+	if Cofactor(a, 3, 2) != 105 {
+		t.Error("wrong value")
+	}
+	if b[2][3] != 105.0/532.0 {
+		t.Error("wrong value")
+	}
+
+	expectedString := `
+| 0.21805 | 0.45113 | 0.24060 | -0.04511 |
+| -0.80827 | -1.45677 | -0.44361 | 0.52068 |
+| -0.07895 | -0.22368 | -0.05263 | 0.19737 |
+| -0.52256 | -0.81391 | -0.30075 | 0.30639 |`
+
+	expected, err := MatrixFromString(expectedString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !AreEqual(b, expected) {
+		t.Error("matrices should be equal")
+	}
+
+	aString = `
+| 8 | -5 | 9 | 2 |
+| 7 | 5 | 6 | 1 |
+| -6 | 0 | 9 | 6 |
+| -3 | 0 | -9 | -4 |`
+
+	expectedString = `
+| -0.15385 | -0.15385 | -0.28205 | -0.53846 |
+| -0.07692 | 0.12308 | 0.02564 | 0.03077 |
+| 0.35897 | 0.35897 | 0.43590 | 0.92308 |
+| -0.69231 | -0.69231 | -0.76923 | -1.92308 |`
+
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+	expected, err = MatrixFromString(expectedString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !AreEqual(expected, a.Inverse()) {
+		t.Error("matrices should be equal")
+	}
+
+	aString = `
+| 9 | 3 | 0 | 9 |
+| -5 | -2 | -6 | -3 |
+| -4 | 9 | 6 | 4 |
+| -7 | 6 | 6 | 2 |`
+
+	expectedString = `
+| -0.04074 | -0.07778 | 0.14444 | -0.22222 |
+| -0.07778 | 0.03333 | 0.36667 | -0.33333 |
+| -0.02901 | -0.14630 | -0.10926 | 0.12963 |
+| 0.17778 | 0.06667 | -0.26667 | 0.33333 |`
+
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+	expected, err = MatrixFromString(expectedString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !AreEqual(expected, a.Inverse()) {
+		t.Error("matrices should be equal")
+	}
+
+	aString = `
+| 3 | -9 | 7 | 3 |
+| 3 | -8 | 2 | -9 |
+| -4 | 4 | 4 | 1 |
+| -6 | 5 | -1 | 1 |`
+
+	bString := `
+| 8 | 2 | 2 | 2 |
+| 3 | -1 | 7 | 0 |
+| 7 | 0 | 5 | 4 |
+| 6 | -2 | 0 | 5 |`
+
+	a, err = MatrixFromString(aString)
+	if err != nil {
+		t.Error(err)
+	}
+	b, err = MatrixFromString(bString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	c := Multiply(a, b)
+
+	if !AreEqual(a, Multiply(c, b.Inverse())) {
+		t.Error("matrices should be equal")
 	}
 }
